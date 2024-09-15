be66737a6fa8ce88325bbdb41c9f743beee00318
ad-itya07 <jhaaditya2207@gmail.com>
2024-09-16 02:48:49 +0530

    minor changes

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -2,5 +2,6 @@ import passport from 'passport';
 
 export const loginAuthenticate = passport.authenticate("local" , {
     successRedirect: '/auth/profile',
+    failureRedirect: '/login',
     failureMessage: 'Authentication failed!',
 });
