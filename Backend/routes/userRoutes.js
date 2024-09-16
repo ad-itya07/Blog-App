ff81fb4d1a36c2440063d650bef90eae64af8eee
ad-itya07 <jhaaditya2207@gmail.com>
2024-09-16 20:18:51 +0530

    minor changes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import userController , {upload} from "../controllers/userController";
+import userController , {upload} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
